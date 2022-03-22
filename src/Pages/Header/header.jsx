a4c5e30643a7418f30aa007b9ff9410a144dfba8
zdeep10 <gabriel.wti02@gmail.com>
2022-03-22 02:58:19 -0300

    Construindo Header

--- src/Pages/Header/header.jsx
+++ src/Pages/Header/header.jsx
@@ -1,7 +1,29 @@
-import './header.scss'
+import "./header.scss";
+import logo from "../../Assets/img/Logo.png";
+import sala from "../../Assets/img/02.png";
 
 const Header = () => {
-  return <h1>Esta dando certo</h1>;
+  return (
+    <header className="header__container">
+      <nav className="nav__container">
+        <img src={logo} alt="Logo Coopers" />
+        <button>Entrar</button>
+      </nav>
+      <div className="header__flex">
+        <div className="header__title">
+          <h1>Organize</h1>
+          <p>your daily jobs</p>
+
+          <p>The only way to get things done</p>
+
+          <button>Go to TO-do list</button>
+        </div>
+        <div className="header__sala">
+          <img src={sala} alt="" />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
