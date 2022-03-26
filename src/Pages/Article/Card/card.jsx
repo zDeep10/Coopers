4a87eb4e0f52140d9cbccb21217e90df00dda6e5
zdeep10 <gabriel.wti02@gmail.com>
2022-03-26 18:17:17 -0300

    Adiconando novos cards

--- src/Pages/Article/Card/card.jsx
+++ src/Pages/Article/Card/card.jsx
@@ -2,11 +2,18 @@ import cooIcon from "../../../Assets/icons/icon-coopers.png";
 import bitmap from "../../../Assets/img/bitmap2.png";
 
 const Card = ({ item }) => {
+  const imgPath = "./src/Assets/img" + item.img;
+
   return (
     <div className="article__card">
       <div className="article__img">
-        <img className="radius" src={bitmap} alt="Daily jobs" loading="lazy"/>
-        <img className="position" src={cooIcon} alt="Coopers Icon" loading="lazy" />
+        <img className="radius" src={imgPath} alt="Daily jobs" loading="lazy" />
+        <img
+          className="position"
+          src={cooIcon}
+          alt="Coopers Icon"
+          loading="lazy"
+        />
       </div>
 
       <div className="article__info">
