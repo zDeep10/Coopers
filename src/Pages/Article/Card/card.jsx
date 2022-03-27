26f75887a3d0d48a3d1dd566efdc5987ceab3662
zdeep10 <gabriel.wti02@gmail.com>
2022-03-26 21:32:55 -0300

    '

--- src/Pages/Article/Card/card.jsx
+++ src/Pages/Article/Card/card.jsx
@@ -1,14 +1,15 @@
 import cooIcon from "../../../Assets/icons/icon-coopers.png";
 
 const Card = ({ item }) => {
-  let imgPath = "/assets/img" + item.img;
-
-  console.log(imgPath)
-
   return (
     <div className="article__card">
       <div className="article__img">
-        <img className="radius" src={imgPath} alt="Daily jobs" loading="lazy" />
+        <img
+          className="radius"
+          src={"/src/img" + item.img}
+          alt="Daily jobs"
+          loading="lazy"
+        />
 
         <img
           className="position"
